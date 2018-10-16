97a1642c613ddc92980195e15ebf95ba7b63aae3
William Gao <gaowhui@gmail.com>
2018-10-16 16:44:07 -0700

    unit test 2

--- unit-tester/src/app/04-forms/todo-form.component.spec.ts
+++ unit-tester/src/app/04-forms/todo-form.component.spec.ts
@@ -1,15 +1,22 @@
-import { TodoFormComponent } from './todo-form.component'; 
+import { FormBuilder } from '@angular/forms';
+import { TodoFormComponent } from './todo-form.component';
+
 
 describe('TodoFormComponent', () => {
-  var component: TodoFormComponent; 
+  let component: TodoFormComponent;
 
   beforeEach(() => {
-
+    component = new TodoFormComponent(new FormBuilder());
   });
 
-  it('', () => {
+  it('should create form with 2 controls', () => {
+    expect(component.form.contains('name')).toBe(true);
+    expect(component.form.contains('email')).toBe(true);
   });
 
-  it('', () => {
+  it('should validiate name field', () => {
+    const control = component.form.get('name');
+    control.setValue('');
+    expect(control.valid).toBeFalsy();
   });
 });
