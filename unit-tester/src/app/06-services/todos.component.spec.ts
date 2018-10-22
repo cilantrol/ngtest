3b6fa3b0de0bc7443b394640418c2eed0175222e
William Gao <gaowhui@gmail.com>
2018-10-22 15:35:42 -0700

    v3

--- unit-tester/src/app/06-services/todos.component.spec.ts
+++ unit-tester/src/app/06-services/todos.component.spec.ts
@@ -1,12 +1,26 @@
-import { TodosComponent } from './todos.component'; 
-import { TodoService } from './todo.service'; 
+import { Observable } from 'rxjs/internal/Observable';
+import { TodosComponent } from './todos.component';
+import { TodoService } from './todo.service';
+import { from } from 'rxjs/observable/from';
 
 describe('TodosComponent', () => {
   let component: TodosComponent;
+  let service: TodoService;
 
   beforeEach(() => {
+    service = new TodoService(null);
+    component = new TodosComponent(service);
   });
 
-  it('', () => {
+  it('should set todos property with the items returned from the server', () => {
+    spyOn(service, 'getTodos').and.callFake(() => {
+      // return Observable.from([ [1, 2, 3] ]);
+      return from([ [1, 2, 3] ]);
+      // return Observable.from( [1, 2]);
+    });
+
+    component.ngOnInit();
+
+    expect(component.todos.length).toBe(3);
   });
 });
