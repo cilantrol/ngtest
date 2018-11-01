19bc1a5b2553e4cebdabc206fa00fa994df99798
unknown <gaowhui@gmail.com>
2018-11-01 12:10:25 -0700

    unit test v1

---
+++ src/app/1-voter/voter.component.spec.ts
@@ -0,0 +1,30 @@
+import { By } from '@angular/platform-browser';
+import { VoterComponent } from './voter.component';
+import { TestBed, ComponentFixture } from '@angular/core/testing';
+
+
+describe('VoterComponent', () => {
+  let component: VoterComponent;
+  let fixture: ComponentFixture<VoterComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [ VoterComponent ]
+    });
+    fixture = TestBed.createComponent(VoterComponent);
+    component = fixture.componentInstance;
+    // fixture.nativeElement;
+    // fixture.debugElement;
+  });
+
+  it('should render total votes', () => {
+    component.othersVote = 20;
+    component.myVote = 1;
+    fixture.detectChanges();
+    // fixture.debugElement.query(By.directive(VoterComponent));
+    const de = fixture.debugElement.query(By.css('.vote-count'));
+    const el: HTMLElement = de.nativeElement;
+
+    expect(el.innerText).toContain('21');
+  });
+});
